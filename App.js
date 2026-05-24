77f38fd345ccd8b8b6c257127a4c9d86cfbbca3e
taynaracluz <taynaracluz@gmail.com>
2026-05-23 22:35:12 -0300

    feat: conexão com ExpoGo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.helloText}>Hello World! 🚀</Text>
+      <Text style={styles.subText}>Meu aplicativo de fidelidade está vivo.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212', // Um fundo escuro elegante
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  helloText: {
+    color: '#fff',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  subText: {
+    color: '#888',
+    fontSize: 16,
   },
 });
